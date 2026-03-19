808900f510c2199791d5a20022e0a5088863994e
Rohan Shah <moreinmore@gmail.com>
2026-03-19 18:49:31 +0530

    rename variables to something relevant

--- src/types/css-processor-types.ts
+++ src/types/css-processor-types.ts
@@ -1,5 +1,7 @@
 import { Rule } from 'postcss'
 
+// CORE will be used to track those classes that are mentioned in the JSX
+// OTHERS will be used to track those classes that are not mentioned in the JSX
 type RuleType = 'CORE' | 'OTHER'
 
 type RuleMap = Map<string, Rule>
